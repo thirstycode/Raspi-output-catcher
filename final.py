dc607232f5c455eac546d8d2a1d43ab3f723143b
Karan Singh Bhakuni [3rd] <pratikkinage@gmail.com>
2018-06-08 22:51:41 +0530

    Update final.py

--- final.py
+++ final.py
@@ -2,7 +2,7 @@ from git import push_to_github
 from write import write_md
 from datetime import datetime as dt
 
-token = "c54480e5ce0c442dfc37d110b79e31a9807a6a4a"
+token = ""
 filename="README.md"
 repo = "thirstycode/Raspi-output-catcher"
 
