c83578bafff8f7bc3d5e3c1e4c9e5be3a443b965
Pratik Kinage <pratikkinage@gmail.com>
2018-06-12 00:37:17 +0530

    Delete write.py

--- write.py
+++
@@ -1,7 +0,0 @@
-def write_md(string):
-	with open("README.md","r") as thefile:
-		content = thefile.readlines()
-	content.insert(2,"-	" + string + "\n")
-	with open("README.md","w") as thefile:
-		for item in content:
-			thefile.write("%s" % item)
